2b5020782ce8e9f3eaed007faee736231f25e69f
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-09-30 22:52:30 -0400

    Adding code to FlashList.jsx
    adding my onclick and flip

---
+++ Flashcards/src/assets/components/FlashcardList.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Flashcard from './Flashcard';
+import { flashcards } from '../data/flashcards';
+
+function FlashcardList() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    const newIndex = Math.floor(Math.random() * flashcards.length);
+    setCurrentIndex(newIndex);
+  };
+
+  const currentCard = flashcards[currentIndex];
+
+  return (
+    <div className="flashcard-list">
+      <h1> Data Structures Flashcards</h1>
+      <p>Description: Review fundamental in data structures you will look over: Definitions, Use Cases, Time Complexity, Common Operations, Comparison, Behavior --> Structure!</p>
+      <p>Total Cards: {flashcards.length}</p>
+
+      <Flashcard card={currentCard} />
+
+      <button onClick={handleNext}>Next Random Card</button>
+    </div>
+  );
+}
+
+export default FlashcardList;
+//Definition Flashcards
+//Use Case Flashcards
+// Time Complexity Flashcards
+//Common Operations Flashcards
+// Comparison Flashcards
+//Behavior -> Structure 
