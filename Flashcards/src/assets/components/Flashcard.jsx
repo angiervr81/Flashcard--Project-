74e8936e062fa25c6b1f310abc0b5e2281d35d75
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-10-07 16:13:24 -0400

    Update Flashcard.jsx

--- Flashcards/src/assets/components/Flashcard.jsx
+++ Flashcards/src/assets/components/Flashcard.jsx
@@ -1,29 +1,29 @@
 import React, { useState } from 'react';
 
+function Flashcard({ card, flipped, onFlip }) {
+  const images = import.meta.glob('../images/*', { eager: true });
 
-const images = import.meta.glob('../images/*',{eager: true});
-function Flashcard({ card }) {
-  const [flipped, setFlipped] = useState(false);
-
-  const handleFlip = () => {
-    setFlipped(!flipped);
-  };
-
-const imagePath = card.image
- ? Object.keys(images).find((keys) => keys.includes(card.image))
+  const imagePath = card.image 
+  ? Object.keys(images).find((key) => key.includes(card.image))
   : null;
-
-  return (
-    <div className="flashcard" onClick={handleFlip}>
+  
+  return(
+    <div className="flashcard" onClick={onFlip}>
       <div className={`card-inner ${flipped ? 'flipped' : ''}`}>
         <div className="card-front">
           <p>{card.question}</p>
         </div>
         <div className="card-back">
-          <div className="card-answer">
-            <p>{card.answer}</p>
-              {imagePath && <img src={images[imagePath].default} alt="Card Visual" className="card-image" />}
-          </div>
+         <div className="card-answer">
+          <p>{card.answer}</p>
+            {imagePath && (
+              <img
+                src={images[imagePath].default}
+                alt="Card Visual"
+                className="card-image"
+              />
+            )}
+        </div>
         </div>
       </div>
     </div>
